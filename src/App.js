a7609a7c64e8e80511a71a5c8c96ae90f5952359
Andreas Benckert <andreas.benckert@gmail.com>
2022-08-23 11:51:37 +0200

    Tailwind working

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import "./input.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 class="text-5xl font-bold underline">Hello world!</h1>
+    <div className="wrapper">
+      <nav>
         <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      </nav>
+      <div className="hero">
+        <h1 className="header">
+          Tailwind CSS makes styling React components easier!
+        </h1>
+      </div>
     </div>
   );
 }
