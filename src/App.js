293ede2dcb547791c80b61cf5bcda3f237ccc977
Andreas Benckert <andreas.benckert@gmail.com>
2022-08-25 16:31:29 +0200

    Header complete

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ function App() {
   return (
     <div className="wrapper">
       <nav className="bg-secondary">
-        <img src={logo} className="logo" alt="logo" />
+        <iframe
+          src="https://my.spline.design/car-a5ed1f319853ce80ff6c99216c7caf81/"
+          frameborder="0"
+          width="100%"
+          height="100%"
+        ></iframe>
       </nav>
       <div className="hero">
         <h1 className="header">
