4b111c3709160910c3d7978e8f03f605f7612f9b
Andreas Benckert <andreas.benckert@gmail.com>
2022-08-30 12:27:57 +0200

    Server working

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mysql = require("mysql8");
+const cors = require("cors");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Pernilla91",
+  database: "sys",
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/get", (req, res) => {
+  const sqlGet = "SELECT * FROM right2driveDB";
+  db.query(sqlGet, (error, result) => {
+    res.send(result);
+  });
+});
+
+// test works
+// app.get("/", (req, res) => {
+//   const sqlInsert =
+//     "INSERT INTO right2driveDB (name, email, phone, place) VALUES ('Bobby', 'Bob@gmail.com', '123456789', 'Under')";
+//   db.query(sqlInsert, (error, result) => {
+//     console.log("error", error);
+//     console.log("result", result);
+//     res.send("Hello Express");
+//   });
+// });
+
+//by deafult express server run under home route,we have to define this route , Cannot GET /
+app.listen(4000, () => {
+  console.log("server is listening on port 4000");
+});
