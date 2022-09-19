6b6ac888e8d408d9830fdba518b2da9e29ba25ca
Kim Michael Garcia Haas <Kgh13690@gmail.com>
2022-09-19 13:10:50 +0200

    Commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { GlobalStyles } from "./global-styles";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    < GlobalStyles />
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
