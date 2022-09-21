44b499429b1da2a44d796a8b5628995693636603
Andreas Benckert <andreas.benckert@gmail.com>
2022-09-21 14:44:51 +0200

    Reaptcha

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-
 import FormInput from "./components/FormInput.js";
 import "./App.css";
 import "./input.css";
 import "./components/form.css";
 
-
-
-
 import Spline from "@splinetool/react-spline";
 import { useState } from "react";
 import { FooterContainer } from "./containers/footer.js";
 
+import Reaptcha from "reaptcha";
 
 
 function App() {
-  const [first, setFirst] = useState(true);
-  const [second, setSecond] = useState(true);
-
+  
+  const [first] = useState(true);
+  const [second] = useState(true);
 
   const handleChange = (data) => {
     console.log(data);
@@ -107,40 +104,45 @@ function App() {
   return (
     <div className="wrapper">
       <nav className="bg-gradient-to-r from-cyan-400 to-blue-400">
-     
-   
         <Spline
           className="car"
           scene="https://prod.spline.design/xXq89CAzYxe9LNrY/scene.splinecode"
         />
       </nav>
       <div className="hero">
-        <h1 className="header">
-         
-        </h1>
         <form onSubmit={handleSubmit}>
-        <h1 className="register">Registrera dig</h1>
-        {inputs.map((input) => (
-          <FormInput
-            key={input.id}
-            {...input}
-            value={values[input.name]}
-            onChange={onChange}
-            
-          />
-          
-        ))}
+          <h1 className="register">Registrera dig</h1>
+          {inputs.map((input) => (
+            <FormInput
+              key={input.id}
+              {...input}
+              value={values[input.name]}
+              onChange={onChange}
+            />
+          ))}
           <div className="App">
-            <input className="checkbox" type="checkbox" value={first} onChange={() => handleChange("First")} /> Email
-            <input className="checkbox1" type="checkbox" value={second} onChange={() => handleChange("Second")} /> Sms
-            <div className="g-recaptcha" data-sitekey="6Lfh-xoeAAAAAL6D9bn_LABq10M1ZtpYup5-wksY"></div>
-            
+            <input
+              className="checkbox"
+              type="checkbox"
+              value={first}
+              onChange={() => handleChange("First")}
+            />{" "}
+            Email
+            <input
+              className="checkbox1"
+              type="checkbox"
+              value={second}
+              onChange={() => handleChange("Second")}
+            />{" "}
+            Sms
           </div>
-        <button>Submit</button>
+        <Reaptcha
+          sitekey="6Lfh-xoeAAAAAL6D9bn_LABq10M1ZtpYup5-wksY"
+        />
+
+          <button>Submit</button>
         </form>
-      
       </div>
-   
       <FooterContainer />
     </div>
   );
