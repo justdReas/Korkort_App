a21f5c59fd482d3b5a48db505559408452450880
Andreas Benckert <andreas.benckert@gmail.com>
2022-09-21 10:19:42 +0200

    No FIREBASE

--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GlobalStyles } from "./global-styles";
 
-// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
-const functions = require('firebase-functions');
-
-// The Firebase Admin SDK to access Firestore.
-const admin = require('firebase-admin');
-admin.initializeApp();
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
